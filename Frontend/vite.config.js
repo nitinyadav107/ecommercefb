1cc765f037defe9cce8063fd1b0de1cd74017ff6
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2025-01-14 01:18:12 +0530

    save

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,5 +6,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
-  }
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+    },
+  },
 })
