3e8875d26f43d781f31a63bc0a791c8653f4c2e9
nitinyadav107 <160407157+nitinyadav107@users.noreply.github.com>
2025-01-21 20:11:53 +0530

    save

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -210,7 +210,7 @@ const verifyRazorpay = async (req, res) => {
           "Payment Successful"
       })
     } else {
-      await orderModel.deleteOne({ _id: orderInfo.receipt, payment: false });
+      await orderModel.deleteOne({ razorpay_order_id: razorpay_order_id });
       res.json({
         success: false, message:
           'Payment Failed'
